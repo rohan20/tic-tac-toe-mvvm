e77df32395831c217f2fc3d33cc8b9beacb806f0
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-05 11:13:46 +0530

    added packages and model class Player

---
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/model/Player.java
@@ -0,0 +1,16 @@
+package com.rohantaneja.tictactoe_mvvm.model;
+
+/**
+ * Created by rohantaneja on 05/04/18.
+ */
+
+public class Player {
+
+    private String name;
+    private String value;
+
+    public Player(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+}
--- app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/MainActivity.java
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.rohantaneja.tictactoe_mvvm;
+package com.rohantaneja.tictactoe_mvvm.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.rohantaneja.tictactoe_mvvm.R;
 
 public class MainActivity extends AppCompatActivity {
 
