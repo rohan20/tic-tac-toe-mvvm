f8dd7a7cedc22273d14e3b9437a24eec003809c6
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-06 13:28:11 +0530

    added GameViewModel class

---
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/viewmodel/GameViewModel.java
@@ -0,0 +1,10 @@
+package com.rohantaneja.tictactoe_mvvm.viewmodel;
+
+import java.util.Observable;
+
+/**
+ * Created by rohantaneja on 06/04/18.
+ */
+
+public class GameViewModel extends Observable {
+}
