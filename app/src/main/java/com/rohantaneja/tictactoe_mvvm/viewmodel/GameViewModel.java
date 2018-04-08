a8362b32eb03fb3988ca2f656172b32c1e712ee5
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-08 14:50:45 +0530

    added constructor for GameViewModel to initialise a game

--- app/src/main/java/com/rohantaneja/tictactoe_mvvm/viewmodel/GameViewModel.java
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/viewmodel/GameViewModel.java
@@ -1,5 +1,8 @@
 package com.rohantaneja.tictactoe_mvvm.viewmodel;
 
+import com.rohantaneja.tictactoe_mvvm.model.Game;
+import com.rohantaneja.tictactoe_mvvm.model.Player;
+
 import java.util.Observable;
 
 /**
@@ -7,4 +10,18 @@ import java.util.Observable;
  */
 
 public class GameViewModel extends Observable {
+
+    private static final String TAG = GameViewModel.class.getName();
+    private Game game;
+
+    public GameViewModel(String player1Name, String player2Name) {
+        game = new Game();
+        beginGame(new Player(player1Name, "X"), new Player(player2Name, "O"));
+    }
+
+    private void beginGame(Player player1, Player player2) {
+        game.setPlayer1(player1);
+        game.setPlayer2(player2);
+        game.setCurrentPlayer(player1);
+    }
 }
