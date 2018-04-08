d7e34d78a5c1227242f87d0f129322a0161f05fa
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-09 02:51:25 +0530

    made MainActivity implement Observer interface to subscribe to changes from the viewmodel

--- app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/MainActivity.java
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/MainActivity.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 
 import com.rohantaneja.tictactoe_mvvm.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainActivity extends AppCompatActivity implements Observer {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void update(Observable observable, Object winnerName) {
+
     }
 }
