585727c338192e8147f7dd05a6f81d39b4f34621
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-10 14:45:53 +0530

    init databinding in MainActivity and setup ViewModel

--- app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/MainActivity.java
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/MainActivity.java
@@ -1,20 +1,43 @@
 package com.rohantaneja.tictactoe_mvvm.view;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.rohantaneja.tictactoe_mvvm.R;
+import com.rohantaneja.tictactoe_mvvm.databinding.ActivityMainBinding;
+import com.rohantaneja.tictactoe_mvvm.viewmodel.GameViewModel;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class MainActivity extends AppCompatActivity implements Observer {
 
+    private static final String GAME_BEGIN_DIALOG_TAG = "game_begin_dialog_fragment";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        promptForPlayers();
+    }
+
+    private void promptForPlayers() {
+        GameBeginDialogFragment gameBeginDialogFragment = GameBeginDialogFragment.newInstance(this);
+        gameBeginDialogFragment.show(getFragmentManager(), GAME_BEGIN_DIALOG_TAG);
+    }
+
+    public void onPlayersSet(String player1Name, String player2Name) {
+        initDataBinding(player1Name, player2Name);
+    }
+
+    private void initDataBinding(String player1Name, String player2Name) {
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        GameViewModel gameViewModel = new GameViewModel(player1Name, player2Name);
+        mainBinding.setGameViewModel(gameViewModel);
+        gameViewModel.addObserver(this);
     }
 
     @Override
@@ -22,7 +45,4 @@ public class MainActivity extends AppCompatActivity implements Observer {
 
     }
 
-    public void onPlayersSet(String player1Name, String player2Name) {
-        Toast.makeText(this, "Player 1: " + player1Name + "\nPlayer 2:" + player2Name, Toast.LENGTH_SHORT).show();
-    }
 }
