3bd683ec6df29fd3c5c699ccd3d5dbd11daf5b56
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-10 14:22:02 +0530

    added complete implementation for GameBeginDialogFragment

--- app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/GameBeginDialogFragment.java
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/GameBeginDialogFragment.java
@@ -6,6 +6,10 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -22,8 +26,14 @@ public class GameBeginDialogFragment extends DialogFragment {
     private LinearLayout rootView;
     private MainActivity activity;
 
+    private TextInputLayout player1TextInputLayout;
+    private TextInputLayout player2TextInputLayout;
+
     private TextInputEditText player1NameEditText;
     private TextInputEditText player2NameEditText;
+
+    String player1Name;
+    String player2Name;
 
     public static GameBeginDialogFragment newInstance(MainActivity activity) {
         GameBeginDialogFragment dialogFragment = new GameBeginDialogFragment();
@@ -34,9 +44,7 @@ public class GameBeginDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        rootView = (LinearLayout) LayoutInflater.from(activity).inflate(R.layout.game_begin_dialog, rootView);
-        player1NameEditText = rootView.findViewById(R.id.player_1_name_edit_text);
-        player2NameEditText = rootView.findViewById(R.id.player_2_name_edit_text);
+        initViews();
 
         final AlertDialog alertDialog = new AlertDialog.Builder(activity)
                 .setView(rootView)
@@ -57,6 +65,18 @@ public class GameBeginDialogFragment extends DialogFragment {
         return alertDialog;
     }
 
+    private void initViews() {
+        rootView = (LinearLayout) LayoutInflater.from(activity).inflate(R.layout.game_begin_dialog, rootView);
+
+        player1TextInputLayout = rootView.findViewById(R.id.player_1_text_input_layout);
+        player2TextInputLayout = rootView.findViewById(R.id.player_2_text_input_layout);
+
+        player1NameEditText = rootView.findViewById(R.id.player_1_name_edit_text);
+        player2NameEditText = rootView.findViewById(R.id.player_2_name_edit_text);
+
+        addTextWatchers();
+    }
+
     private void onDialogShow(AlertDialog alertDialog) {
         Button dialogDoneButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
         dialogDoneButton.setOnClickListener(new View.OnClickListener() {
@@ -68,5 +88,69 @@ public class GameBeginDialogFragment extends DialogFragment {
     }
 
     private void onDoneClicked() {
+
+        if (isValidPlayerName(player1TextInputLayout, player1Name) && isValidPlayerName(player2TextInputLayout, player2Name)) {
+            activity.onPlayersSet(player1Name, player2Name);
+            dismiss();
+        }
     }
+
+    private boolean isValidPlayerName(TextInputLayout textInputLayout, String playerName) {
+
+        if (TextUtils.isEmpty(playerName)) {
+            textInputLayout.setErrorEnabled(true);
+            textInputLayout.setError("Player name cannot be empty");
+            return false;
+        }
+
+        if (player1Name.equalsIgnoreCase(player2Name)) {
+            textInputLayout.setErrorEnabled(true);
+            textInputLayout.setError("Player names cannot be same");
+            return false;
+        }
+
+        textInputLayout.setErrorEnabled(false);
+        textInputLayout.setError("");
+        return true;
+    }
+
+    private void addTextWatchers() {
+
+        player1NameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                player1Name = editable.toString();
+            }
+        });
+
+        player2NameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                player2Name = editable.toString();
+            }
+        });
+
+    }
+
 }
+
--- app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/MainActivity.java
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/view/MainActivity.java
@@ -2,6 +2,7 @@ package com.rohantaneja.tictactoe_mvvm.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.rohantaneja.tictactoe_mvvm.R;
 
@@ -19,5 +20,9 @@ public class MainActivity extends AppCompatActivity implements Observer {
     @Override
     public void update(Observable observable, Object winnerName) {
 
+    }
+
+    public void onPlayersSet(String player1Name, String player2Name) {
+        Toast.makeText(this, "Player 1: " + player1Name + "\nPlayer 2:" + player2Name, Toast.LENGTH_SHORT).show();
     }
 }
