86c7421505d53207ced083a06e8dc6a3d0f26901
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-10 16:17:58 +0530

    added ObservableArrayMap to display text after cell click

--- app/src/main/java/com/rohantaneja/tictactoe_mvvm/viewmodel/GameViewModel.java
+++ app/src/main/java/com/rohantaneja/tictactoe_mvvm/viewmodel/GameViewModel.java
@@ -1,8 +1,11 @@
 package com.rohantaneja.tictactoe_mvvm.viewmodel;
 
+import android.databinding.ObservableArrayMap;
+
 import com.rohantaneja.tictactoe_mvvm.model.Cell;
 import com.rohantaneja.tictactoe_mvvm.model.Game;
 import com.rohantaneja.tictactoe_mvvm.model.Player;
+import com.rohantaneja.tictactoe_mvvm.util.StringUtil;
 
 import java.util.Observable;
 
@@ -14,6 +17,7 @@ public class GameViewModel extends Observable {
 
     private static final String TAG = GameViewModel.class.getName();
     private Game game;
+    public ObservableArrayMap<String, String> cellValuesMap = new ObservableArrayMap<>();
 
     public GameViewModel(String player1Name, String player2Name) {
         game = new Game();
@@ -29,6 +33,8 @@ public class GameViewModel extends Observable {
     public void onClickedCellAt(int row, int column) {
         Cell[][] cell = game.getCells();
         cell[row][column] = new Cell(game.getCurrentPlayer());
+
+        cellValuesMap.put(StringUtil.stringFromNumbers(row, column), game.getCurrentPlayer().getValue());
 
         if (game.hasGameEnded()) {
             onGameHasEnded();
